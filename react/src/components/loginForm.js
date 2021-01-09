fa0b2331fa1abe270297ec040132edd5fd32fce8
MySecoundLanguage <mmamunism@gmail.com>
2021-01-09 22:44:57 +0600

    Brough login inside div

--- react/src/components/loginForm.js
+++ react/src/components/loginForm.js
@@ -1,28 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 function LoginForm() {
-    return (
-        <form>
-            <div className="form-group">
-                <label htmlFor="username">Username</label>
-                <input
-                    type="email"
-                    className="form-control"
-                    id="username"
-                    aria-describedby="emailHelp"
-                    placeholder="Enter username"/>
-            </div>
-            <div className="form-group">
-                <label htmlFor="epassword">Password</label>
-                <input
-                    type="password"
-                    className="form-control"
-                    id="epassword"
-                    placeholder="Password"/>
-            </div>
+    const [formData, setFormData] = useState({
+        username: '',
+        password: '',
+      });
 
-            <button className="btn btn-primary">Login</button>
-        </form>
+
+    const onChangeHandler = (e) => {
+        formData[e.target.name] = e.target.value
+        setFormData(formData)
+      }
+
+    const onSubmitHandler = (e) => {
+        e.preventDefault()
+        console.log(formData)
+    }
+
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12">
+                    <div>
+                    <form onSubmit={onSubmitHandler}>
+                        <div className="form-group">
+                            <label htmlFor="username">Username</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="username"
+                                aria-describedby="emailHelp"
+                                name="username"
+                                onChange={onChangeHandler}
+                                placeholder="Enter username"/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="epassword">Password</label>
+                            <input
+                                type="password"
+                                className="form-control"
+                                id="epassword"
+                                name="password"
+                                onChange={onChangeHandler}
+                                placeholder="Password"/>
+                        </div>
+
+                        <button className="btn btn-primary">Login</button>
+                    </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
